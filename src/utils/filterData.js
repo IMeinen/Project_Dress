7ed9f1be900ef6657cfecd62cc95b36e1deb6a5b
arturmeinen <arturmeinen@gmail.com>
2020-05-31 18:00:34 -0300

    add bias to filter

--- src/utils/filterData.js
+++ src/utils/filterData.js
@@ -1,30 +1,68 @@
 import { FilterOptions } from '../constants/FilterOptions';
 
+// ORDENA POR PONTOS
+const orderByPoints = ( a, b ) => {
+  if ( a.points > b.points ){
+    return -1;
+  }
+  if ( a.points < b.points ){
+    return 1;
+  }
+  return 0;
+}
+
+
 export const filterData = (data) => {
-  const res = FilterOptions.filter((item) => {
-    return (
-      item.location.includes(data.location) &&
-      item.schedule.includes(data.schedule) &&
-      item.season.includes(data.season) &&
-      (item.color.includes(data.color) || data.color === 'branco,off') &&
-      item.structure.includes(data.structure) &&
-      (item.toppings.includes(data.toppings) ||
-        (data.toppings === 'limpo,liso' &&
-          (item.toppings.includes('liso') ||
-            item.toppings.includes('liso')))) &&
-      item.body.includes(data.body)
-    );
+  const res = FilterOptions.map((item,index) => {
+    let points = 0;
+
+    // DETALHES
+    if (
+      item.toppings.includes(data.toppings) ||
+      (data.toppings === 'limpo,liso' &&
+        (item.toppings.includes('liso') || item.toppings.includes('liso')))
+    ) {
+      points += 1.5;
+    }
+    // ESTRUTURA
+    if (item.structure.includes(data.structure)) {
+      points += 1.5;
+    }
+    // LOCAL
+    if(item.location.includes(data.location)){
+      points += 1.4;
+    }
+    // COR
+    if((item.color.includes(data.color) || data.color === 'branco,off')){
+      points += 1.3;
+    }
+     // TIPO DE CORPO
+     if(item.body.includes(data.body)){
+      points += 1.2;
+    }
+       // ESTAÇÃO
+       if(item.season.includes(data.season)){
+        points += 1.1;
+      }
+    // HORÁRIOS
+    if(item.schedule.includes(data.schedule)){
+      points += 1;
+    }
+
+    item.points = points;
+
+
+    return item;
   });
 
-  return res;
+  return  res.sort(orderByPoints).slice(0,4);
 };
 
 
+// CHECA SE O ESTADO FOI PREENCHIDO
 export const checkData = (data) => {
-
-  if (Object.values(data).indexOf('') > -1){
+  if (Object.values(data).indexOf('') > -1) {
     return false;
   }
   return true;
 };
-
